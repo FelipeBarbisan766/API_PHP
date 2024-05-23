626f4973e21f8c533a7bc056b8a2d5959f14b5b8
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-05-23 11:17:41 -0300

    atualização

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema_crud</title>
 </head>
+<?php 
+include_once("navBar.php")
+?>
 <body>
 <h1>Sistema Crud</h1>
-<a href="Cliente/forminserir.php">Novo usuario</a>
-<a href="Cliente/listar.php">Listar usuario</a>
+<a href="Cliente/forminserir.php"class="btn btn-primary">Novo usuario</a>
+<a href="Cliente/listar.php"class="btn btn-primary">Listar usuario</a>
     
 </body>
 </html>
